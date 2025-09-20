ca3fba4649bf043212ef50f456ce5a8a3b73cf21
YasasSri2002 <srimedagedara234@gmail.com>
2025-09-20 18:45:12 +0530

    hero/background img

--- src/components/ui/hero.tsx
+++ src/components/ui/hero.tsx
@@ -1,7 +1,13 @@
+'use client'
 export default function HeroSection(){
     return(
-        <div className="bg-white w-dvw h-dvh ">
-            <img src="hero.jpg" alt="" />
+        <div className="w-full h-dvh ">
+            <div className="bg-[url(/hero.jpg)] bg-size-[100%_100%] bg-center bg-fixed bg-no-repeat w-dvw sm:w-full h-dvh">
+            <div className="bg-black/60 w-full h-full z-10"></div>
+            <div className="w-full h-full grid items-center justify-items-center text-white/80 absolute top-0">
+            <h1 className="text-6xl">Urbanite Hotel</h1>
+            </div>
+            </div>
         </div>
     )
 } 
