5646e37230e2ac1d19f14089ae6eecf4d03121a2
YasasSri2002 <srimedagedara234@gmail.com>
2025-09-29 11:11:41 +0530

    add contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 
-import Image from "next/image";
 import HeroSection from "@/components/ui/hero-section/hero";
 import NavBar from "../components/ui/nav-bar/navbar";
 import AboutSection from "@/components/ui/about-section/about";
 import RoomSection from "@/components/ui/rooms-section/rooms";
-import DropdownMenu from "@/components/ui/hero-section/dropdown";
 import AmenitiesSection from "@/components/ui/amenties-section/amenties";
+import ContactSection from "@/components/ui/get-In-touch-section/contact";
 
 
 
@@ -19,6 +18,7 @@ export default function Home() {
      <AboutSection/>
      <RoomSection/>
      <AmenitiesSection/>
+     <ContactSection/>
     </>
   );
 }
--- src/components/ui/get-In-touch-section/contact.tsx
+++ src/components/ui/get-In-touch-section/contact.tsx
@@ -1,7 +1,17 @@
+import { HeaderModel } from "@/components/models/header";
+import HeaderSection from "../header";
+
 export default function ContactSection(){
+
+    const headerDetails:HeaderModel ={
+        sectionTopic: "Contact Us",
+        topic: "Get in Touch",
+        details: "Have questions about your stay or need assistance with booking? Our friendly team is here to help you 24/7.",
+    }
+
     return(
         <>
-            <div></div>
+            <HeaderSection details={headerDetails}/>
         </>
     );
 }
