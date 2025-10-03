243816e0e9aca618c5b356bd77726af51fd72c59
YasasSri2002 <srimedagedara234@gmail.com>
2025-10-03 17:20:27 +0530

    api call to backend to get rooms

---
+++ src/components/api-calls/room-api.tsx
@@ -0,0 +1,21 @@
+'use client';
+import axios from "axios";
+import { useEffect,useState } from "react";
+import { RoomModel } from "../models/roomModel";
+export default function RoomAPI(){
+
+    const [room,setRoom] =useState<RoomModel[]>([]);
+
+    useEffect(()=>{
+        const getRooms = async()=>{
+        const response = await axios.get<RoomModel[]>('http://localhost:8080/api/v1/admin/get-room');
+        setRoom(response.data);
+    }
+    getRooms();
+},[]
+
+)
+
+
+    return({room});
+}
---
+++ src/components/models/roomModel.ts
@@ -0,0 +1,6 @@
+export interface RoomModel{
+    id: number;
+    type: string;
+    description: string;
+    price: number;
+}
